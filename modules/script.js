390e1f71060dd97774261789133b503f4a3a4dda
ATOJ5 <dgc105@gmail.com>
2023-11-21 21:57:48 +0100

    adding async/away to the code

--- modules/script.js
+++ modules/script.js
@@ -5,64 +5,77 @@ const NEWGAME = new GAME.game();
 const STARTNEWGAME = document.getElementById("play");
 const TRYNUMBER = document.getElementById('tryNumber');
 const TRYNUMBER_INPUT = document.getElementById('tryNumberInput');
-const DISPLAY1= document.getElementById('display1');
-const DISPLAY2= document.getElementById('display2');
-const DISPLAY3= document.getElementById('display3');
+const DISPLAY1 = document.getElementById('display1');
+const DISPLAY2 = document.getElementById('display2');
+const DISPLAY3 = document.getElementById('display3');
 const GORILA = document.getElementById('gorila');
 const CHARACTER = document.getElementById('character');
-const TITLE = document.getElementById('title')
+const TITLE = document.getElementById('title');
+
 let operation1 = new Operations();
 let operation2 = new Operations();
-var time = 5000;
+let timeInterval = 5000;
+
+let currentStep = 0;
+let move;
+let gorilaX = GORILA.getBoundingClientRect().x + GORILA.getBoundingClientRect().width / 2;
+let characterX = CHARACTER.getBoundingClientRect().x + CHARACTER.getBoundingClientRect().width / 2;
+let steps = 7;
+let distance = characterX - gorilaX;
+let stepSize = distance / steps;
 
 STARTNEWGAME.addEventListener('click', () => {
-    
-    TITLE.innerHTML = "Good Luck"
+    TITLE.innerHTML = "Good Luck";
     DISPLAY1.innerHTML = operation1.getNumber();
     DISPLAY2.innerHTML = operation2.getNumber();
-    let gorilaX = GORILA.getBoundingClientRect().x + GORILA.getBoundingClientRect().width / 2;
-    let characterX = CHARACTER.getBoundingClientRect().x + CHARACTER.getBoundingClientRect().width / 2;
-    let steps = 7; 
-    let distance = characterX - gorilaX;
-    let stepSize = distance / steps;
-    let currentStep = 0;
 
-    setTimeout(function moveStep() {
-        time = 5000;
-        if (currentStep < steps) {
-            generateNumbers();
-            gorila.style.transform = `translateX(${gorilaX + stepSize * currentStep}px)`;
-            currentStep++;
-            setTimeout(moveStep, time); 
-        } else if (currentStep === steps) {
-            CHARACTER.style.transition = 'transform 1s'; 
-            CHARACTER.style.transform = 'rotate(3600deg) translateY(-500px)';
-            setTimeout(() =>{
-                TITLE.innerHTML = '!!YOU LOSE!!'
-            }, 500);
-        }
-    }, time); 
-    
+    handleMoveStep();
 });
 
-TRYNUMBER.addEventListener('click', () => {
-    
-    let userInput = parseInt(TRYNUMBER_INPUT.value);
-    
-    if (operation1.getNumber() + operation2.getNumber() == userInput){
-        NEWGAME.player.points += 1; 
-        DISPLAY3.innerHTML = NEWGAME.player.points;
-        generateNumbers();
-        time +=1000;
-    } else {
-        time -=2000;
-        //no logro conseguir el efecto deseado
+    async function moveStep() {
+        return new Promise((resolve, reject) => {
+            move = setInterval(() => {
+                if (currentStep < steps) {
+                    generateNumbers();
+                    gorila.style.transform = `translateX(${gorilaX + stepSize * currentStep}px)`;
+                    currentStep++;
+                } else {
+                    reject(clearInterval(move));
+                    lose();
+                }
+            }, timeInterval);
+        });
     }
-});
 
-function generateNumbers(){
-    operation1.generateRandomNumber();
-    DISPLAY1.innerHTML = operation1.getNumber();
-    operation2.generateRandomNumber();
-    DISPLAY2.innerHTML = operation2.getNumber();
-}
+    async function changeInterval() {
+        let userInput = parseInt(TRYNUMBER_INPUT.value);
+        if (operation1.getNumber() + operation2.getNumber() === userInput) {
+            NEWGAME.player.points += 1;
+            DISPLAY3.innerHTML = NEWGAME.player.points;
+            generateNumbers();
+        } 
+    }
+
+    async function handleMoveStep() {
+        while (currentStep < steps) {
+            await moveStep();
+        }
+    }
+
+    TRYNUMBER.addEventListener('click', async () => {
+        await changeInterval(); 
+    });
+
+    function generateNumbers() {
+        operation1.generateRandomNumber();
+        DISPLAY1.innerHTML = operation1.getNumber();
+        operation2.generateRandomNumber();
+        DISPLAY2.innerHTML = operation2.getNumber();
+    }
+
+    function lose(){
+        CHARACTER.style.transition = 'transform 1s'; 
+        CHARACTER.style.transform = 'rotate(3600deg) translateY(-500px)';
+        TITLE.innerHTML = '!!YOU LOSE!!';
+    }
+
